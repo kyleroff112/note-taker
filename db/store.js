045a0543ac16bf8565211125065b2ced3e8f8151
kyleroff112 <kyleroff112@gmail.com>
2023-03-28 01:43:37 -0400

    added code from tutor session

---
+++ db/store.js
@@ -0,0 +1,54 @@
+const util = require('util');
+const fs = require('fs');
+
+const { v4: uuidv4 } = require('uuid');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+class Store {
+  read() {
+    return readFile('db/db.json', 'utf8');
+  }
+
+  write(note) {
+    return writeFile('db/db.json', JSON.stringify(note));
+  }
+
+  getNotes() {
+    return this.read().then((notes) => {
+      let parsedNotes;
+
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        parsedNotes = [];
+      }
+
+      return parsedNotes;
+    });
+  }
+
+  addNote(note) {
+    const { title, text } = note;
+
+    if (!title || !text) {
+      throw new Error("Note 'title' and 'text' cannot be blank");
+    }
+
+    const newNote = { title, text, id: uuidv4() };
+
+    return this.getNotes()
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => newNote);
+  }
+
+  removeNote(id) {
+    return this.getNotes()
+      .then((notes) => notes.filter((note) => note.id !== id))
+      .then((filteredNotes) => this.write(filteredNotes));
+  }
+}
+
+module.exports = new Store();
---
+++ server.js
@@ -0,0 +1,14 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
